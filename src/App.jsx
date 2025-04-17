3ae617e3f8b60ea4011b9bcdb13c692c547fefb4
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-17 10:24:28 +0600

    created blog fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
     <>
       <div>
         <Navbar></Navbar>
+        <Blogs></Blogs>
       </div>
     </>
   );

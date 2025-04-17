6f1417b85683ecd4daf768204cca5c8ccced1435
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-17 10:06:16 +0600

    project initislized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/NavBar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  );
+}
+
+export default App;
